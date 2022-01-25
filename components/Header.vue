b33c929920e744ff1041527613efde035618b7bf
appleRui <apple31581@gmail.com>
2022-01-25 17:25:19 +0900

    Feature/220125/Header

---
+++ components/Header.vue
@@ -0,0 +1,32 @@
+<template>
+  <div class="header">
+    <div class="container">
+      <div class="ttl-content">
+        <h1 class="ttl">都道府県別の総人口推移グラフ</h1>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style scoped>
+.header {
+  background-color: #f5f5f5;
+  padding: 16px;
+  position: sticky;
+  top: 0;
+  left: 0;
+}
+.ttl-content {
+  display: flex;
+  justify-content: center;
+}
+.ttl-content > .ttl {
+  font-size: 18px;
+}
+</style>
--- nuxt.config.js
+++ nuxt.config.js
@@ -11,24 +11,38 @@ export default {
     htmlAttrs: {
       lang: 'en'
     },
-    meta: [
-      { charset: 'utf-8' },
-      { name: 'viewport', content: 'width=device-width, initial-scale=1' },
-      { hid: 'description', name: 'description', content: '' },
-      { name: 'format-detection', content: 'telephone=no' }
+    meta: [{
+        charset: 'utf-8'
+      },
+      {
+        name: 'viewport',
+        content: 'width=device-width, initial-scale=1'
+      },
+      {
+        hid: 'description',
+        name: 'description',
+        content: ''
+      },
+      {
+        name: 'format-detection',
+        content: 'telephone=no'
+      }
     ],
-    link: [
-      { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }
-    ]
+    link: [{
+      rel: 'icon',
+      type: 'image/x-icon',
+      href: '/favicon.ico'
+    }]
   },
 
   // Global CSS: https://go.nuxtjs.dev/config-css
   css: [
+    '@/assets/common.css',
+    '@/assets/reset.css'
   ],
 
   // Plugins to run before rendering page: https://go.nuxtjs.dev/config-plugins
-  plugins: [
-  ],
+  plugins: [],
 
   // Auto import components: https://go.nuxtjs.dev/config-components
   components: true,
@@ -49,6 +63,5 @@ export default {
   axios: {},
 
   // Build Configuration: https://go.nuxtjs.dev/config-build
-  build: {
-  }
+  build: {}
 }
