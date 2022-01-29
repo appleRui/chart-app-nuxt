eda8f74e5474b8d42ebb09aba95a0e5e9cd2a590
appleRui <apple31581@gmail.com>
2022-01-29 19:12:17 +0900

    Fix/220129/Common Mount Index From index.spec.js

--- test/index.spec.js
+++ test/index.spec.js
@@ -3,18 +3,17 @@ import {
 } from '@vue/test-utils';
 import Index from '@/pages/index';
 
+const wrapper = mount(Index)
+
 describe('Index', () => {
   describe('component', () => {
     test('Display Header', () => {
-      let wrapper = mount(Index)
       expect(wrapper.vm).toBeTruthy;
     });
     test('Display Prefectures', () => {
-      let wrapper = mount(Index)
       expect(wrapper.vm).toBeTruthy;
     });
     test('Feach Prefectures From RESAS API', () => {
-      let wrapper = mount(Index)
       expect(wrapper.vm.$props);
     });
   });
